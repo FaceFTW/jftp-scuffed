a9a91a25dd8c6777716392b9e082cbdb0ff96caa
Robb <computerguy440+gh@gmail.com>
2022-09-04 15:04:40 -0400

    Turn off basically everything in the CSSE375 test class, because the build path isn't configured with junit stuff yet

--- src/java/yourTestsHere/CSSE375Test.java
+++ src/java/yourTestsHere/CSSE375Test.java
@@ -1,66 +1,63 @@
 package yourTestsHere;
 
-import static org.junit.Assert.*;
-
-import net.sf.jftp.JFtp;
-import net.sf.jftp.gui.base.LocalDir;
-import net.sf.jftp.gui.base.RemoteDir;
-
-import org.junit.Before;
-import org.junit.Test;
+//import net.sf.jftp.gui.base.LocalDir;
+//import net.sf.jftp.gui.base.RemoteDir;
+//import org.junit.Before;
+//import org.junit.Test;
+//
+//import static org.junit.Assert.fail;
 
 /**
  * Put your unit tests in this class.
  * Put any Testing-only subclasses in this file or in the yourTestsHere package
- * 
- * @author hewner
  *
+ * @author hewner
  */
 public class CSSE375Test {
 
-	@Before
-	public void setUp() throws Exception {
-	}
-	
-/*
- Requirements for tests (from the assignment):
- 
-+ You *must not* have your tests rely on either a particular webserver
-  (e.g. ftp.csse.rose-hulman.edu) OR the local filesystem.  Instead,
-  you need to create a fake connection with non-real files and test
-  your remote and local dirs with that.
+//    @Before
+//    public void setUp() throws Exception {
+//    }
 
-+ You must create both LocalDir and RemoteDir in your tests (or maybe
-  a test specific subclass or either of them).
+    /*
+     Requirements for tests (from the assignment):
 
-+ The code you test should be the actual code in those classes, not
-  some copy of the code in your test-only classes.
+    + You *must not* have your tests rely on either a particular webserver
+      (e.g. ftp.csse.rose-hulman.edu) OR the local filesystem.  Instead,
+      you need to create a fake connection with non-real files and test
+      your remote and local dirs with that.
 
-+ Avoid changing the codebase to allow for tests (the occasional
-  extract method or extract interface is fine though)
+    + You must create both LocalDir and RemoteDir in your tests (or maybe
+      a test specific subclass or either of them).
 
-+ Your tests should test with several different directory sizes.
+    + The code you test should be the actual code in those classes, not
+      some copy of the code in your test-only classes.
 
-+ Your tests should not bring up a visible GUI
+    + Avoid changing the codebase to allow for tests (the occasional
+      extract method or extract interface is fine though)
 
-+ You should not use any additional testing library (e.g. mocking)
-  beyond JUnit
- 
-+ To test your feature, you can feel free to add a function like
-  getDirLength() and test against that.  Obviously, in the GUI you
-  must use java to bring up a dialog but you don't need to test the
-  dialog creation code.
+    + Your tests should test with several different directory sizes.
 
-*/
-	@Test
-	public void test() {
-		
-		// JFtp jftp = new JFtp(); //this line is problematical because it opens the GUI
-		
-		LocalDir local = new LocalDir();
-		RemoteDir bar = new RemoteDir();
-		
-		fail("Not yet implemented");
-	}
+    + Your tests should not bring up a visible GUI
+
+    + You should not use any additional testing library (e.g. mocking)
+      beyond JUnit
+
+    + To test your feature, you can feel free to add a function like
+      getDirLength() and test against that.  Obviously, in the GUI you
+      must use java to bring up a dialog but you don't need to test the
+      dialog creation code.
+
+    */
+//    @Test
+//    public void test() {
+//
+//        // JFtp jftp = new JFtp(); //this line is problematical because it opens the GUI
+//
+//        LocalDir local = new LocalDir();
+//        RemoteDir bar = new RemoteDir();
+//
+//        fail("Not yet implemented");
+//    }
 
 }
